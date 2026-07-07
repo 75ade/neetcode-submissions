e42e0d6ab85341bbd391435863843a3305af20c3
Nguyen Trong Dang Khoa <113686668+75ade@users.noreply.github.com>
2026-07-07 13:35:41 +0700

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let str;
+        let map = new Map();
+        let res = [];
+        let temp;
+
+        for (let i = 0; i < strs.length; i++){
+            str = strs[i].split('').sort().join('');
+
+            if (!map.has(str)){
+                map.set(str, new Array(strs[i]));
+            }
+            else {
+                map.get(str).push(strs[i]);
+            }
+        }
+
+        for (const key of map.keys()){
+            res.push(map.get(key));
+        }
+
+        return res;
+    }
+}
