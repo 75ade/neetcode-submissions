fb3e5e5ea5ada87cd6e75e23dd31780a7f0c159e
Nguyen Trong Dang Khoa <113686668+75ade@users.noreply.github.com>
2026-07-13 13:50:10 +0700

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    minEatingSpeed(piles, h) {
+        piles.sort((a, b) => b - a);
+
+        let left = 1;
+        let right = piles[0];
+        let middle;
+        let kMin = Number.POSITIVE_INFINITY;
+        let eatTime;
+
+        while (left <= right){
+            middle = left + Math.floor((right - left) / 2);
+            eatTime = 0;
+
+            for (let b of piles){
+                eatTime += middle > b ? 1 : Math.ceil(b / middle);    
+            }
+
+            if (eatTime <= h){
+                if (middle < kMin)   kMin = middle;
+                right = middle - 1;
+            }
+            else if (eatTime > h)   left = middle + 1;
+        }
+
+        return kMin;
+    }
+}
