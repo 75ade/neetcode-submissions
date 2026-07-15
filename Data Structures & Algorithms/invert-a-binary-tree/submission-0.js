0008b85f83770341f6f2cba9c31d7ecec393a615
Nguyen Trong Dang Khoa <113686668+75ade@users.noreply.github.com>
2026-07-15 11:23:40 +0700

    Add: invert-a-binary-tree - submission-0

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-0.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {TreeNode}
+     */
+    invertTree(root) {
+        // Queue for BFS
+        if (root === null)  return null;
+        let queue = [root];
+        
+        while (queue.length > 0){
+            const node = queue.shift();
+            [node.left, node.right] = [node.right, node.left];
+            if (node.left !== null) queue.push(node.left);
+            if (node.right !== null)    queue.push(node.right);
+        }
+        
+        return root;
+    }
+}
