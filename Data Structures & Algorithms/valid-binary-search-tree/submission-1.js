82186dcccd9e55fa7d1841663f1ca3a8d115f9b0
Nguyen Trong Dang Khoa <113686668+75ade@users.noreply.github.com>
2026-07-17 11:51:48 +0700

    Add: valid-binary-search-tree - submission-1

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-1.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {boolean}
+     */
+    isValidBST(root) {
+        let queue = [[root, Number.NEGATIVE_INFINITY, Number.POSITIVE_INFINITY]];
+
+        while (queue.length > 0){
+            const [node, left, right] = queue.shift();
+
+            if (!(left < node.val && node.val < right)) return false;
+
+            if (node.left !== null){
+                queue.push([node.left, left, node.val]);
+            }
+
+            if (node.right !== null){
+                queue.push([node.right, node.val, right]);
+            }
+        }
+
+        return true;
+    }
+}
